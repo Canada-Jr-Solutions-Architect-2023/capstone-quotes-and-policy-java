1fdc9cb81b8534ba0cf5b8e5f08e6024da94034f
Vyas <2297123@cognizant.com>
2023-09-18 21:58:16 -0400

    Spring project created, database connected

---
+++ src/main/java/com/lizardostrich/quoteandpolicymanagement/QuoteAndPolicyManagementApplication.java
@@ -0,0 +1,13 @@
+package com.lizardostrich.quoteandpolicymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuoteAndPolicyManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuoteAndPolicyManagementApplication.class, args);
+	}
+
+}
